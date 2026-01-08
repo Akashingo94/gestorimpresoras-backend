0f037ba4ae37dc49a8dc2ee60a7d4ac1bf03bb49
Enickma <Benitezmiguel747@gmail.com>
2026-01-08 19:22:16 -0300

    feat: Implementar dominio local personalizado gestorimpresoras.local

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,11 +1,19 @@
 /**
  * CORS Configuration
  * Configuración de Cross-Origin Resource Sharing
+ * Permite acceso desde localhost y dominio local personalizado
  */
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
-  credentials: true // Permitir el envío de cookies
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'http://gestorimpresoras.local:3000',
+    'http://127.0.0.1:3000'
+  ],
+  credentials: true, // Permitir el envío de cookies
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 };
 
 module.exports = corsOptions;
